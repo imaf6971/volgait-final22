550aa8e4ed711db2aa72beac226f21d8db2b0802
Vladislav Mironov <imaf6971@gmail.com>
2022-09-28 22:48:42 +0300

    renamed UserController to AuthenticationController and using AuthenticationService

--- src/main/java/xyz/imaf6971/volgaitfinal/controllers/AuthenticationController.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/controllers/AuthenticationController.java
@@ -5,25 +5,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.imaf6971.volgaitfinal.dto.RegistrationDto;
-import xyz.imaf6971.volgaitfinal.service.UserService;
+import xyz.imaf6971.volgaitfinal.service.AuthenticationService;
 
 @RestController
 @RequestMapping("/api/v1")
-public class UserController {
+public class AuthenticationController {
 
-    private final UserService userService;
+    private final AuthenticationService auth;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public AuthenticationController(AuthenticationService auth) {
+        this.auth = auth;
     }
 
     @PostMapping("/registration")
     public void registration(@RequestBody RegistrationDto registrationDto) {
-        userService.register(registrationDto);
+        auth.register(registrationDto);
     }
 
     @PostMapping("/login")
     public void login(@RequestBody RegistrationDto registrationDto) {
-        userService.login(registrationDto);
+        auth.login(registrationDto);
     }
 }
