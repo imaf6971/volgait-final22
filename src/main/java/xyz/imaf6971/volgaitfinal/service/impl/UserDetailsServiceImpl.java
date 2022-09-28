d751401a0f241500786cc77eb8062cfd61ff845e
Vladislav Mironov <imaf6971@gmail.com>
2022-09-28 14:25:13 +0300

    implemented roles in UserDetailsServiceImpl

--- src/main/java/xyz/imaf6971/volgaitfinal/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/service/impl/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package xyz.imaf6971.volgaitfinal.service.impl;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,7 +9,8 @@ import org.springframework.stereotype.Service;
 import xyz.imaf6971.volgaitfinal.model.User;
 import xyz.imaf6971.volgaitfinal.repository.UserRepository;
 
-import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -26,7 +29,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private UserDetails toUserDetails(User user) {
         return new org.springframework.security.core.userdetails.User(
-                user.getUsername(), user.getPassword(), new ArrayList<>());
+                user.getUsername(), user.getPassword(), rolesToAuthorities(user));
+    }
+
+    private Collection<? extends GrantedAuthority> rolesToAuthorities(User user) {
+        var authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().getTitle());
+        return Collections.singletonList(authority);
     }
 
     private User getByUsername(String username) {
