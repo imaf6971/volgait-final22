924bdffed9383ad2ff9f8c791153889ea110daf9
Vladislav Mironov <imaf6971@gmail.com>
2022-09-28 22:51:00 +0300

    refactored UserService and reimplemented UserServiceImpl

--- src/main/java/xyz/imaf6971/volgaitfinal/service/UserService.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/service/UserService.java
@@ -1,12 +1,13 @@
 package xyz.imaf6971.volgaitfinal.service;
 
-import xyz.imaf6971.volgaitfinal.dto.RegistrationDto;
 import xyz.imaf6971.volgaitfinal.model.User;
 
 public interface UserService {
-    void register(RegistrationDto registrationDto);
-
-    void login(RegistrationDto registrationDto);
 
     User getCurrentUser();
+
+    User getByUsername(String username);
+
+    void saveUser(User user);
+
 }
--- src/main/java/xyz/imaf6971/volgaitfinal/service/impl/UserServiceImpl.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/service/impl/UserServiceImpl.java
@@ -1,59 +1,19 @@
 package xyz.imaf6971.volgaitfinal.service.impl;
 
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import xyz.imaf6971.volgaitfinal.dto.RegistrationDto;
 import xyz.imaf6971.volgaitfinal.model.User;
 import xyz.imaf6971.volgaitfinal.repository.UserRepository;
-import xyz.imaf6971.volgaitfinal.service.RoleService;
 import xyz.imaf6971.volgaitfinal.service.UserService;
 
-import java.util.Collection;
-import java.util.Collections;
-
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService {
+public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository;
+    private final UserRepository repository;
 
-    private final PasswordEncoder passwordEncoder;
-
-    private final AuthenticationManager authenticationManager;
-
-    private final RoleService roleService;
-
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager, RoleService roleService) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.authenticationManager = authenticationManager;
-        this.roleService = roleService;
-    }
-
-    @Override
-    public void register(RegistrationDto registrationDto) {
-        var user = new User();
-        user.setRole(roleService.getDefaultRole());
-        user.setUsername(registrationDto.username());
-        user.setPassword(passwordEncoder.encode(registrationDto.password()));
-        userRepository.save(user);
-    }
-
-    @Override
-    public void login(RegistrationDto registrationDto) {
-        var authToken = new UsernamePasswordAuthenticationToken(
-                registrationDto.username(),
-                registrationDto.password()
-        );
-        var auth = authenticationManager.authenticate(authToken);
-        SecurityContextHolder.getContext().setAuthentication(auth);
+    public UserServiceImpl(UserRepository repository) {
+        this.repository = repository;
     }
 
     @Override
@@ -63,24 +23,14 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var user = getByUsername(username);
-        return toUserDetails(user);
-    }
-
-    private UserDetails toUserDetails(User user) {
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(), user.getPassword(), rolesToAuthorities(user));
-    }
-
-    private Collection<? extends GrantedAuthority> rolesToAuthorities(User user) {
-        var authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().getTitle());
-        return Collections.singletonList(authority);
-    }
-
-    private User getByUsername(String username) {
-        return userRepository.findByUsername(username)
+    public User getByUsername(String username) {
+        return repository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+    }
+
+    @Override
+    public void saveUser(User user) {
+        repository.save(user);
     }
 
 }
