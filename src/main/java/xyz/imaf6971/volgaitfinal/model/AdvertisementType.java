8fca3bab2762a9372f2864ab10d82a07e40a60b3
Vladislav Mironov <imaf6971@gmail.com>
2022-09-30 13:39:08 +0300

    added advertisements property to AdvertisementType

--- src/main/java/xyz/imaf6971/volgaitfinal/model/AdvertisementType.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/model/AdvertisementType.java
@@ -2,7 +2,9 @@ package xyz.imaf6971.volgaitfinal.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Entity
 @Table(name = "advertisement_types")
@@ -10,6 +12,9 @@ public class AdvertisementType extends AbstractEntity {
 
     @Column(name = "title", nullable = false, unique = true)
     private String title;
+
+    @OneToMany(mappedBy = "type")
+    private Set<Advertisement> advertisements;
 
     public String getTitle() {
         return title;
@@ -19,4 +24,11 @@ public class AdvertisementType extends AbstractEntity {
         this.title = title;
     }
 
+    public Set<Advertisement> getAdvertisements() {
+        return advertisements;
+    }
+
+    public void setAdvertisements(Set<Advertisement> advertisements) {
+        this.advertisements = advertisements;
+    }
 }
