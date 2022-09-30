39f8159671c40b0b6e5ccc9a6cc423f8cd56b08d
Vladislav Mironov <imaf6971@gmail.com>
2022-09-30 12:34:38 +0300

    added typeId to AdvertisementDto

--- src/main/java/xyz/imaf6971/volgaitfinal/dto/AdvertisementDto.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/dto/AdvertisementDto.java
@@ -1,4 +1,8 @@
 package xyz.imaf6971.volgaitfinal.dto;
 
-public record AdvertisementDto(Long id, String title, String text) {
+public record AdvertisementDto(
+        Long id,
+        String title,
+        String text,
+        Long typeId) {
 }
