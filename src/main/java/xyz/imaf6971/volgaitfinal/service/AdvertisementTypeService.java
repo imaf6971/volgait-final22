d22a98b1ec93be904bd47c7385d5f2de64b196af
Vladislav Mironov <imaf6971@gmail.com>
2022-09-30 13:40:00 +0300

    added more methods to AdvertisementTypeService and implemented it

--- src/main/java/xyz/imaf6971/volgaitfinal/service/AdvertisementTypeService.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/service/AdvertisementTypeService.java
@@ -1,9 +1,21 @@
 package xyz.imaf6971.volgaitfinal.service;
 
+import xyz.imaf6971.volgaitfinal.dto.AdvertDto;
+import xyz.imaf6971.volgaitfinal.dto.AdvertisementTypeDto;
 import xyz.imaf6971.volgaitfinal.model.AdvertisementType;
+
+import java.util.List;
 
 public interface AdvertisementTypeService {
 
+    AdvertisementTypeDto getTypeDtoById(Long typeId);
+
     AdvertisementType getTypeById(Long typeId);
+
+    List<AdvertisementTypeDto> getTypes();
+
+    List<AdvertDto> getTypeAdvertsById(Long typeId);
+
+    void addType(AdvertisementTypeDto advertisementTypeDto);
 
 }
--- src/main/java/xyz/imaf6971/volgaitfinal/service/impl/AdvertisementTypeServiceImpl.java
+++ src/main/java/xyz/imaf6971/volgaitfinal/service/impl/AdvertisementTypeServiceImpl.java
@@ -1,10 +1,15 @@
 package xyz.imaf6971.volgaitfinal.service.impl;
 
 import org.springframework.stereotype.Service;
+import xyz.imaf6971.volgaitfinal.dto.AdvertDto;
+import xyz.imaf6971.volgaitfinal.dto.AdvertisementTypeDto;
 import xyz.imaf6971.volgaitfinal.exception.AdvertisementTypeNotFoundException;
+import xyz.imaf6971.volgaitfinal.model.Advertisement;
 import xyz.imaf6971.volgaitfinal.model.AdvertisementType;
 import xyz.imaf6971.volgaitfinal.repository.AdvertisementTypeRepository;
 import xyz.imaf6971.volgaitfinal.service.AdvertisementTypeService;
+
+import java.util.List;
 
 @Service
 public class AdvertisementTypeServiceImpl implements AdvertisementTypeService {
@@ -16,9 +21,51 @@ public class AdvertisementTypeServiceImpl implements AdvertisementTypeService {
     }
 
     @Override
+    public AdvertisementTypeDto getTypeDtoById(Long typeId) {
+        return toDto(getTypeById(typeId));
+    }
+
+    @Override
     public AdvertisementType getTypeById(Long typeId) {
         return typeRepository.findById(typeId)
                 .orElseThrow(() -> new AdvertisementTypeNotFoundException("Adv type with id " + typeId + "don't exists"));
+    }
+
+    @Override
+    public List<AdvertisementTypeDto> getTypes() {
+        return typeRepository.findAll().stream()
+                .map(this::toDto)
+                .toList();
+    }
+
+    @Override
+    public List<AdvertDto> getTypeAdvertsById(Long typeId) {
+        return getTypeById(typeId)
+                .getAdvertisements().stream()
+                .map(this::toAdvertDto)
+                .toList();
+    }
+
+    @Override
+    public void addType(AdvertisementTypeDto advertisementTypeDto) {
+        var type = new AdvertisementType();
+        type.setTitle(advertisementTypeDto.title());
+        typeRepository.save(type);
+    }
+
+    private AdvertDto toAdvertDto(Advertisement advertisement) {
+        return new AdvertDto(
+                advertisement.getId(),
+                advertisement.getTitle(),
+                advertisement.getText()
+        );
+    }
+
+    private AdvertisementTypeDto toDto(AdvertisementType advertisementType) {
+        return new AdvertisementTypeDto(
+                advertisementType.getId(),
+                advertisementType.getTitle()
+        );
     }
 
 }
